7672437df5d99e3ee9456a35781da7ad24fc4440
lekanda <abernaolao@gmail.com>
2020-02-28 15:39:13 +0100

    13:Creando el servidor express

---
+++ inde.js
@@ -0,0 +1,8 @@
+// import express from 'express';// Esta no la soporta NODE 
+const express= require('express');
+
+// Crear un aplicacion express
+const app = express();
+
+// Puerto de escucha de express
+app.listen(3000);
