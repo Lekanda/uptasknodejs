d2777c60bbbad1cfd0e14a2a5f8b100e40efc785
lekanda <abernaolao@gmail.com>
2020-03-01 09:29:29 +0100

    17:Creando el controlador de proyectos

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 const express= require('express');
 const routes = require('./routes');
 
-
 // Crear un aplicacion express
 const app = express();
 
 //Habilitar Rutas
 app.use('/', routes() );
-
 
 // Puerto de escucha de express
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// importar el controlador
+const proyectosController = require('../controllers/proyectosController');
 
 module.exports = function() {
     // Ruta para el Home
-    router.get('/', (req, res) => {
-        res.send('hola desde el /index.JS');
-    });
+    router.get('/', proyectosController.proyectosHome);
     router.get('/nosotros', (req, res) => {
         res.send('hola desde el , nosotros');
     });
