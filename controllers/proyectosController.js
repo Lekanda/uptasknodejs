a536c68c8f7cb39615727f5046ed28a11e5d997c
lekanda <abernaolao@gmail.com>
2020-03-01 20:39:12 +0100

    26:Validacion Simple de un Formulario

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -10,6 +10,25 @@ exports.formularioProyecto = (req, res) => {
 }
 exports.nuevoProyecto = (req, res) => {
     // res.send('Enviaste el formulario');
+
     // Enviar a consola lo que el usuario escriba
     console.log(req.body);
+
+    // Validar que tenga algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+    if(!nombre) {
+        errores.push({'texto':'Agrega un Nombre al Proyecto'});
+    }
+
+    // Si hay errores
+    if(errores.length>0) {
+        res.render('nuevo-proyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores
+        })
+    } else {
+        // No hay errores, Insertar en la DB
+    }
 }
