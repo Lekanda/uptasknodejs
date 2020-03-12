c44ec8ad3915f97ef3cec4ca15da17f9d61c984e
lekanda <abernaolao@gmail.com>
2020-03-12 14:35:49 +0100

    53:Enviando una peticion con Axios para eliminar

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -3,28 +3,46 @@ import axios from 'axios';
 
 const btnEliminar = document.querySelector('#eliminar-proyecto');
 
-btnEliminar.addEventListener('click', () => {
-    // console.log('diste click en eliminar');
-    Swal.fire({
-        title: '¿Quieres borrar el Proyecto?',
-        text: "No podras revertir el cambio!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, Borralo!',
-        cancelButtonText: 'No, Cancelar'// Este no sale, hay que ponerlo a mano
-    }).then((result) => {
-    if (result.value) {
-        Swal.fire(
-        'Proyecto Borrado!',
-        'Tu Proyecto se ha sido Borrado.',
-        'success'
-        );
-        //redireccionar al inicio
-        setTimeout(() => {
-            window.location.href = '/'
-        }, 3000);
-    }
+if(btnEliminar) {
+    btnEliminar.addEventListener('click', e => {
+        const urlProyecto = e.target.dataset.proyectoUrl;
+        // console.log('diste click en eliminar');
+        // console.log(urlProyecto);
+
+        // return;
+
+        Swal.fire({
+            title: '¿Quieres borrar el Proyecto?',
+            text: "No podras revertir el cambio!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Borralo!',
+            cancelButtonText: 'No, Cancelar'// Este no sale, hay que ponerlo a mano
+        }).then((result) => {
+        if (result.value) {
+            // Enviar Peticion a Axios
+            const url = `${location.origin}/proyectos/${urlProyecto}`;
+
+            axios.delete(url, {params: urlProyecto})
+                .then(function(respuesta){
+                    console.log(respuesta);
+                    
+                })
+            // console.log(url);
+            return;
+            
+            Swal.fire(
+            'Proyecto Borrado!',
+            'Tu Proyecto se ha sido Borrado.',
+            'success'
+            );
+            //redireccionar al inicio
+            setTimeout(() => {
+                window.location.href = '/'
+            }, 3000);
+        }
+        })
     })
-})
+}
