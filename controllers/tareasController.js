6750868498f030e907d6ddc659c06373baec1820
lekanda <abernaolao@gmail.com>
2020-03-13 19:29:05 +0100

    58:Almacenando las tareas en la DB

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,3 +1,23 @@
-exports.agregarTarea = (req,res) =>{
-    res.send('Enviado');
+const Proyectos = require('../models/Proyectos');
+const Tareas = require('../models/Tareas');
+
+exports.agregarTarea = async (req,res,next) =>{
+    // Obtenemos el Proyecto actual
+    const proyecto = await Proyectos.findOne({where: {url: req.params.url}});
+    console.log(proyecto);
+    // console.log(req.body);
+    // Leer el valor del Input
+    const {tarea} = req.body;
+    // estado 0 = Incompleto y ID de Proyecto
+    const estado = 0;
+    const proyectoId = proyecto.id;
+
+    // Insertar en la DB
+    const resultado = await Tareas.create({ tarea, estado, proyectoId});
+
+    if(!resultado){
+        return next();
+    }
+    // Redireccionar
+    res.redirect(`/proyectos/${req.params.url}`);
 }
