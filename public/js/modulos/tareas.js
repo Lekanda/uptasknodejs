d9d04b1e39bcd1d7e05ca26db195b16c08148986
lekanda <abernaolao@gmail.com>
2020-03-15 10:15:47 +0100

    61: Detectando el ID de la Tarea que se marca como completo

---
+++ public/js/modulos/tareas.js
@@ -0,0 +1,17 @@
+const tareas = document.querySelector('.listado-pendientes');
+
+if (tareas) {
+    tareas.addEventListener('click', (e)=>{
+        // console.log(e.target.classList);
+        if (e.target.classList.contains('fa-check-circle')) {
+            // console.log('Actualizando....');
+            const icono = e.target;
+            const idTarea = icono.parentElement.parentElement.dataset.tarea;
+
+            console.log(idTarea);
+            
+        }
+    });
+}
+
+export default tareas;
