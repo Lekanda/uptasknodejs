d4f5c528cf8f0aeda3c4afe4d5c58a1bbc3b9443
lekanda <abernaolao@gmail.com>
2020-03-25 15:38:25 +0100

    81:Autorizando usuarios(revisar)

--- routes/index.js
+++ routes/index.js
@@ -12,37 +12,71 @@ const authController = require('../controllers/authController');
 
 module.exports = function() {
     // Ruta para el Home
-    router.get('/', proyectosController.proyectosHome);
+    router.get('/',
+        authController.usuarioAutenticado,
+        proyectosController.proyectosHome
+    );
+
     // Rutas para nuevo proyecto
-    router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
+    router.get('/nuevo-proyecto', 
+        authController.usuarioAutenticado,
+        proyectosController.formularioProyecto
+    );
     router.post('/nuevo-proyecto', 
+        authController.usuarioAutenticado,
         body('nombre').not().isEmpty().trim().escape(),
         proyectosController.nuevoProyecto
     );
 
     // Listar Proyecto
-    router.get('/proyectos/:url', proyectosController.proyectoPorUrl);
+    router.get('/proyectos/:url',
+        authController.usuarioAutenticado,
+        proyectosController.proyectoPorUrl
+    );
+
     // Actualizar el Proyecto
-    router.get('/proyecto/editar/:id', proyectosController.formularioEditar);
-    // 
-    router.post('/nuevo-proyecto/:id', 
+    router.get('/proyecto/editar/:id', 
+        authController.usuarioAutenticado,
+        proyectosController.formularioEditar
+    );
+
+    // Crear Nuevo Proyecto
+    router.post('/nuevo-proyecto/:id',
+        authController.usuarioAutenticado,
         body('nombre').not().isEmpty().trim().escape(),
         proyectosController.actualizarProyecto
     );
+
     // Eliminar Proyecto
-    router.delete('/proyectos/:url', proyectosController.eliminarProyecto);
+    router.delete('/proyectos/:url', 
+        authController.usuarioAutenticado,
+        proyectosController.eliminarProyecto
+    );
+
     //Tareas Nuevas
-    router.post('/proyectos/:url' , tareasController.agregarTarea);
+    router.post('/proyectos/:url' , 
+        authController.usuarioAutenticado,
+        tareasController.agregarTarea
+    );
+
     // Actualizar estado de Tarea
-    router.patch('/tareas/:id', tareasController.cambiarEstadoTarea);
+    router.patch('/tareas/:id', 
+        authController.usuarioAutenticado,
+        tareasController.cambiarEstadoTarea
+    );
+
     //Eliminar Tarea
-    router.delete('/tareas/:id', tareasController.eliminarTarea);
+    router.delete('/tareas/:id', 
+        authController.usuarioAutenticado,
+        tareasController.eliminarTarea
+    );
+    
     // Crear nueva cuenta
     router.get('/crear-cuenta', usuariosController.formCrearCuenta);
     router.post('/crear-cuenta', usuariosController.crearCuenta);
     //Iniciar Sesion
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     router.post('/iniciar-sesion', authController.autenticarUsuario);
-
+    
     return router;
 }
