706fa53d37f2157c5378748ddcace224a57fb7d6
lekanda <abernaolao@gmail.com>
2020-03-30 23:05:03 +0200

    prueba 19

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
 app.use('/', routes() );
 
 // Servidor y Puerto
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 
